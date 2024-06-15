55f7643aef1bfa5e6b5533c836d3a0c4bb417aa6
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2024-06-15 15:48:17 +0400

    add: order repository

---
+++ src/infrastructure/orderRepositories/OrderRepository.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "../Database";
+import {IOrderRepository} from "../../interfaces/orderInterfaces";
+import {OrderEntity} from "../../core/entities/orderEntities/OrderEntity";
+
+export class OrderRepository implements IOrderRepository {
+
+  constructor(
+    private readonly dataSource: DataSource,
+  ) { }
+
+  async save(order: OrderEntity): Promise<OrderEntity> {
+    return this.dataSource.orders.save(order);
+  }
+
+  fetchById(id: number): Promise<OrderEntity | undefined> {
+    return this.dataSource.orders.getById(id);
+  }
+}
--- src/interfaces/orderInterfaces.ts
+++ src/interfaces/orderInterfaces.ts
@@ -1,5 +1,10 @@
 import {OrderStatusEntity} from "../core/entities/orderEntities/OrderStatusEntity";
+import {OrderEntity} from "../core/entities/orderEntities/OrderEntity";
 
+export interface IOrderRepository {
+  save(status: OrderEntity): Promise<OrderEntity>;
+  fetchById(id: number): Promise<OrderEntity | undefined>;
+}
 export interface IOrderStatusRepository {
   save(status: OrderStatusEntity): Promise<OrderStatusEntity>;
   fetchById(id: number): Promise<OrderStatusEntity | undefined>;
