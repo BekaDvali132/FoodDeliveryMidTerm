26a716fcaf6c277a87edbb30a1c0843db8e8b935
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2024-06-15 14:43:11 +0400

    update: order item entity init

--- src/core/entities/orderEntities/OrderItemEntity.ts
+++ src/core/entities/orderEntities/OrderItemEntity.ts
@@ -1,10 +1,24 @@
 import { OrderEntity } from "./OrderEntity";
-
+import {ProductEntity} from "../facilityEntities/ProductEntity";
 export class OrderItemEntity {
+    public id: number = 0;
+    public quantity: number = 0;
+
+    public product?: ProductEntity;
+    public order?: OrderEntity;
+
     constructor(
-        public id: number,
-        public order: OrderEntity,
-        public product: string,
-        public amount: number
-    ){}
+        rawData?: Partial<OrderItemEntity>
+    ){
+        if (rawData?.order){
+            this.order = new OrderEntity(rawData.order);
+            delete rawData.order;
+        }
+
+        if (rawData?.product){
+            this.product = new ProductEntity(rawData.product);
+            delete rawData.product;
+        }
+        Object.assign(this, rawData);
+    }
 }
