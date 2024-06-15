b970d59ccb36ee03d572910e71d5c73143c66b09
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2024-06-15 14:21:28 +0400

    update: order entity init

--- src/core/entities/orderEntities/OrderEntity.ts
+++ src/core/entities/orderEntities/OrderEntity.ts
@@ -1,10 +1,17 @@
-import { OrderStatusEntity } from "./OrderStatusEntity";
+import {OrderStatusEntity} from "./OrderStatusEntity";
 
 export class OrderEntity {
-    constructor(
-        public id: number,
-        public status: OrderStatusEntity,
-        public paymentAmount: number
-    ){}
+  public id: number = 0;
+  public paymentAmount: number = 0;
+
+  public status?: OrderStatusEntity;
+
+  constructor(rawData?: Partial<OrderEntity>) {
+    if (rawData?.status) {
+      this.status = new OrderStatusEntity(rawData.status);
+      delete rawData.status;
+    }
+
+    Object.assign(this, rawData);
+  }
 }
-  
