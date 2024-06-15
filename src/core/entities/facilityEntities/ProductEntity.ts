4f7f284651e620943675d48e43ead4bef684266d
Timur Smoev <timur.smoev@rwth-aachen.de>
2024-06-15 14:39:49 +0400

    update: product entity init

--- src/core/entities/facilityEntities/ProductEntity.ts
+++ src/core/entities/facilityEntities/ProductEntity.ts
@@ -2,11 +2,24 @@ import { FacilityEntity } from './FacilityEntity';
 import { Tag } from './Tag';
 
 export class ProductEntity {
-  constructor(
-      public id: number,
-      public name: string,
-      public price: number,
-      public owner: FacilityEntity,
-      public tags: Tag[]
-  ) {}
+  public id: number = 0;
+  public name: string = '';
+  public price: number = 0;
+  
+  public owner?: FacilityEntity;
+  public tags?: Tag[];
+
+  constructor(rawData?: Partial<ProductEntity>) {
+    if (rawData?.owner) {
+      this.owner = new FacilityEntity(rawData.owner);
+      delete rawData.owner;
+    }
+
+    if (rawData?.tags) {
+      this.tags = rawData.tags.map(tag => new Tag(tag))
+      delete rawData.tags;
+    }
+
+    Object.assign(this, rawData);
+  }
 }
