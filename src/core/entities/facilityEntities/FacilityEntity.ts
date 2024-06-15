09c592463a42a3b7ae33f14b934a58518d026b61
Timur Smoev <timur.smoev@rwth-aachen.de>
2024-06-15 14:34:04 +0400

    update: facility entity init

--- src/core/entities/facilityEntities/FacilityEntity.ts
+++ src/core/entities/facilityEntities/FacilityEntity.ts
@@ -2,9 +2,23 @@ import { FacilityType } from './FacilityType';
 import { FacilityManagerEntity } from '../userEntities/FacilityManagerEntity';
 
 export class FacilityEntity {
-  constructor(
-      public id: number,
-      public type: FacilityType,
-      public manager: FacilityManagerEntity,
-  ) {}
+  public id: number = 0;
+
+  public type?: FacilityType;
+  public manager?: FacilityManagerEntity;
+
+  constructor(rawData?: Partial<FacilityEntity>) {
+    if (rawData?.type) {
+      this.type = new FacilityType(rawData.type);
+      delete rawData.type;
+    }
+  
+    if (rawData?.manager) {
+      this.manager = new FacilityManagerEntity(rawData.manager);
+      delete rawData.manager;
+    }
+
+    Object.assign(this, rawData);
+  }
 }
+
