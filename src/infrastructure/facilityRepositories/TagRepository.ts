bc2733bce733fdae13f10380251d278eafb47520
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2024-06-16 12:27:14 +0400

    add: tag repository

---
+++ src/infrastructure/facilityRepositories/TagRepository.ts
@@ -0,0 +1,30 @@
+import {ITagRepository} from "../../interfaces/facilityInterfaces";
+import {Tag} from "../../core/entities/facilityEntities/Tag";
+import {DataSource} from "../Database";
+
+export class TagRepository implements ITagRepository {
+  constructor(
+    private readonly dataSource: DataSource,
+  ) {
+  }
+
+  async save(tag: Tag): Promise<Tag> {
+    const tagNameExists = await this.dataSource.tags.findOne(
+      (item) => item.name === tag.name
+    );
+
+    if (tagNameExists) {
+      throw new Error("Tag name already exists");
+    }
+
+    return this.dataSource.tags.save(tag);
+  }
+
+  fetchById(id: number): Promise<Tag | undefined> {
+    return this.dataSource.tags.getById(id);
+  }
+
+  async update(tag: Tag): Promise<Tag> {
+    return this.dataSource.tags.update(tag);
+  }
+}
