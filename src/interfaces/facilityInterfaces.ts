ec53db0a03fc18392f5666a4a014372ae46c4667
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2024-06-16 12:20:09 +0400

    refactor: facility interfaces in separate interfaces file

---
+++ src/interfaces/facilityInterfaces.ts
@@ -0,0 +1,28 @@
+import {FacilityEntity} from "../core/entities/facilityEntities/FacilityEntity";
+import {FacilityType} from "../core/entities/facilityEntities/FacilityType";
+import {ProductEntity} from "../core/entities/facilityEntities/ProductEntity";
+import {Tag} from "../core/entities/facilityEntities/Tag";
+
+export interface IFacilityRepository {
+  save(facility: FacilityEntity): Promise<FacilityEntity>;
+  fetchById(id: number): Promise<FacilityEntity | undefined>;
+  update(facility: FacilityEntity): Promise<FacilityEntity>;
+}
+
+export interface IFacilityTypeRepository {
+  save(facilityType: FacilityType): Promise<FacilityType>;
+  fetchById(id: number): Promise<FacilityType | undefined>;
+  update(facilityType: FacilityType): Promise<FacilityType>;
+}
+
+export interface IProductRepository {
+  save(product: ProductEntity): Promise<ProductEntity>;
+  fetchById(id: number): Promise<ProductEntity | undefined>;
+  update(product: ProductEntity): Promise<ProductEntity>;
+}
+
+export interface ITagRepository {
+  save(tag: Tag): Promise<Tag>;
+  fetchById(id: number): Promise<Tag | undefined>;
+  update(tag: Tag): Promise<Tag>;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,10 +3,6 @@ import {CustomerEntity} from "../core/entities/userEntities/CustomerEntity";
 import {AdminEntity} from "../core/entities/userEntities/AdminEntity";
 import {CourierEntity} from "../core/entities/userEntities/CourierEntity";
 import {FacilityManagerEntity} from "../core/entities/userEntities/FacilityManagerEntity";
-import { FacilityEntity } from "../core/entities/facilityEntities/FacilityEntity";
-import { FacilityType } from "../core/entities/facilityEntities/FacilityType";
-import { ProductEntity } from "../core/entities/facilityEntities/ProductEntity";
-import { Tag } from "../core/entities/facilityEntities/Tag";
 
 
 export interface IUserRepository {
@@ -34,28 +30,4 @@ export interface ICourierRepository {
 export interface IFacilityManagerRepository {
   save(userId: number, user: FacilityManagerEntity): Promise<FacilityManagerEntity>;
   fetchById(id: number): Promise<FacilityManagerEntity | undefined>;
-}
-
-export interface IFacilityRepository {
-  save(facility: FacilityEntity): Promise<FacilityEntity>;
-  fetchById(id: number): Promise<FacilityEntity | undefined>;
-  update(facility: FacilityEntity): Promise<FacilityEntity>;
-}
-
-export interface IFacilityTypeRepository {
-  save(facilityType: FacilityType): Promise<FacilityType>;
-  fetchById(id: number): Promise<FacilityType | undefined>;
-  update(facilityType: FacilityType): Promise<FacilityType>;
-}
-
-export interface IProductRepository {
-  save(product: ProductEntity): Promise<ProductEntity>;
-  fetchById(id: number): Promise<ProductEntity | undefined>;
-  update(product: ProductEntity): Promise<ProductEntity>;
-}
-
-export interface ITagRepository {
-  save(tag: Tag): Promise<Tag>;
-  fetchById(id: number): Promise<Tag | undefined>;
-  update(tag: Tag): Promise<Tag>;
 }
