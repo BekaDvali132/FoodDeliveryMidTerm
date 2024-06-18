9142a9d9484f0867ac8188f170458226f7d8c098
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2024-06-18 09:57:57 +0400

    add: order status enum

--- src/core/entities/orderEntities/OrderEntity.ts
+++ src/core/entities/orderEntities/OrderEntity.ts
@@ -1,8 +1,16 @@
+export enum orderStatusEnum {
+  pending = 'pending',
+  delivering = 'delivering',
+  preparing = 'preparing',
+  completed = 'completed',
+  cancelled = 'cancelled',
+}
+
 export class OrderEntity {
   public id: number = 0;
   public paymentAmount: number = 0;
 
-  public status: 'pending' | 'delivering' | 'preparing' | 'completed' | 'cancelled' = 'pending'
+  public status: orderStatusEnum = orderStatusEnum.pending
 
   constructor(rawData?: Partial<OrderEntity>) {
     Object.assign(this, rawData);
